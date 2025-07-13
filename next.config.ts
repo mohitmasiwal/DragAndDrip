5cbc40e1ab08f22e8969f57e17de39c56092b240
mohitmasiwal <mohitmasiwal02@gmail.com>
2025-07-13 12:05:50 +0530

    kjbb

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+ // next.config.js
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // ⚠️ disables lint check at build time
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
